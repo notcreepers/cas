685b335bc624be9ac6cc9f41b2eefc4d10d620a8
Creepers <70440007+whycreepers102@users.noreply.github.com>
2021-04-16 14:29:25 -0400

    Bot's first words
    It said "fuck you" and I've never been happier.

---
+++ commands/ping.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'ping',
+    description: "This is ping command idiot you literally coded this how do you not know this.",
+    execute(message, args){
+        message.channel.send('fuck you')
+    }
+}
